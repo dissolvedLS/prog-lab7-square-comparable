2e83f67de6e4caf059b05687077dc98f65cae206
dissolvedLS <deodex@spaces.ru>
2019-05-20 07:05:52 +0300

    Добавьте файлы проекта.

---
+++ prog-lab7-zad3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_lab7_zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // выбор действия
+            int N = 0;
+            Square[] newSquare = new Square[N];
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие: 1 - добавить квадрат, 2 - сортировать, 3 - выход");
+                int action = 0;
+                while (action != 1 && action != 2 && action != 3)
+                {
+                    try
+                    {
+                        action = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Ошибка при вводе! Введите 1, 2 или 3.");
+                        action = 0;
+                    }
+                }
+
+                switch (action)
+                {
+                    case 1:
+                        Console.WriteLine("Введите длину стороны, для создания квадрата : ");
+                        double side = 0;
+                        while (side == 0)
+                        {
+                            try
+                            {
+                                side = Convert.ToDouble(Console.ReadLine());
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Ошибка при вводе! Попытайтесь снова.");
+                                side = 0;
+                            }
+                        }
+
+                        N++;
+                        Console.WriteLine(N);
+                        Array.Resize(ref newSquare, N);
+                        newSquare[N-1] = new Square(side);
+
+                        Console.WriteLine("Квадрат создан!");
+                        Console.WriteLine("Его периметр: " + newSquare[N - 1].Perimeter());
+                        Console.WriteLine("Площадь: " + newSquare[N - 1].Area());
+                        Console.WriteLine("Радиус вписанной окружности: {0:f2}", newSquare[N - 1].r);
+                        Console.WriteLine("Радиус описанной окружности: {0:f2}", newSquare[N - 1].R);
+                        break;
+                    case 2:
+                        Array.Sort(newSquare);
+                        Console.WriteLine("Сортировка прошла успешно!");
+                        foreach (Square sqr in newSquare)
+                            Console.WriteLine("Квадрат со стороной " + sqr.a);
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("-----------------------------------------");
+            }
+        }
+    }
+}
---
+++ prog-lab7-zad3/Square.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace prog_lab7_zad3
+{
+    public interface ICircumcircleIncircle
+    {
+        double R { get; } // радіус описаного кола
+        double r { get; } // радіус вписаного кола
+    }
+    public interface IMeasurable
+    {
+        double Perimeter();
+        double Area();
+    }
+    class Square : IMeasurable, ICircumcircleIncircle, IComparable
+    {
+        public double a;
+
+        public Square()
+        {
+            a = 0;
+        }
+        public Square(double a)
+        {
+            this.a = a;
+        }
+        public double Perimeter()
+        {
+            return a * 4;
+        }
+        public double Area()
+        {
+            return a * a;
+        }
+
+        public double R
+        {
+            get
+            {
+                return Math.Sqrt(a * a + a * a) / 2;
+            }
+        }
+
+        public double r
+        {
+            get
+            {
+                return a / 2;
+            }
+        }
+
+        public int CompareTo(object o)
+        {
+            Square newSqr = o as Square;
+            return this.Perimeter().CompareTo(newSqr.Perimeter());
+        }
+    }
+}
